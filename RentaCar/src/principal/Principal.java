b23b221f674dacd948d2da17fb904f21e6691337
Ernesto Diaz <armadillo288@gmail.com>
2019-06-26 07:32:01 +0200

    splash listo

--- RentaCar/src/principal/Principal.java
+++ RentaCar/src/principal/Principal.java
@@ -32,6 +32,7 @@ import metodos.Metodos;
 import javax.swing.ImageIcon;
 import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
+import javax.swing.UIManager;
 /**
  *
  * @author rls
@@ -55,6 +56,8 @@ public class Principal extends javax.swing.JFrame {
        cl = new GestionBD();
        al= new Metodos();
     }
+    
+  
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -962,6 +965,22 @@ public class Principal extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        try{
+            
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+             Splash f = new Splash();
+             f.setSize(270,270);
+             f.setLocationRelativeTo(null);
+             f.setVisible(true);
+             
+                
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+        
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
